d20293d1baea29a279c964f5562de5ca0240fc6c
Kayleigh <kayodonn@udel.edu>
2026-04-01 10:38:07 -0400

    test

--- app.py
+++ app.py
@@ -22,12 +22,44 @@ if "page" not in st.session_state:
     st.session_state["page"] = "login"
 
 
+users = [
+        {
+        "id": "1",
+        "full_name": "System Admin",
+        "username": "admin1",
+        "password": "123ssag@43AE",
+        "role": "Admin",
+        "registered_at": "..."
+    }
+]
+
+json_path = Path("users.json")
+if json_path.exists():
+    with open(json_path, "r") as f:
+        users = json.load(f)
+
+events = [
+    {
+        "id" : "HW1",
+        "title": "Intro to Database",
+        "description" : "basics of database design",
+        "points": 100,
+        "type" : "homework"
+    }
+]
 
 
+json_path_event = Path("events.json")
 
-if st.session_state["role"] == "User":
+#Load the data from a json file
+if json_path_event.exists():
+    with json_path_event.open("r", encoding= "utf-8") as f:
+        events = json.load(f)
+
+
+if st.session_state["role"] == "Atendee":
     if st.session_state["page"] == "home":
-        st.markdown("Welcome! This is the User Dashboard")
+        st.markdown("Welcome! This is the Attendee Dashboard")
         if st.button("Go to Dashboard", key= "dashboard_view_btn", type= "primary",use_container_width=True):
             st.session_state["page"] = "dashboard"
             st.rerun()
@@ -56,13 +88,30 @@ else:
     # --- LOGIN --
     st.subheader("Log In")
     with st.container(border=True):
-        email_input = st.text_input("Email", key= "email_login")
+        username_input = st.text_input("Username", key= "username_login")
         password_input = st.text_input("Password", type="password",key="password_login")
         
         if st.button("Log In", type="primary", use_container_width=True):
             with st.spinner("Logging in..."):
                 time.sleep(2) # Fake backend delay
-                pass
+                found_user = None
+                for user in users:
+                    if user["username"].strip().lower() == username_input.strip().lower() and user["password"] == password_input:
+                        found_user = user
+                        break
+                
+                if found_user:
+                    st.success(f"Welcome back, {found_user['username']}!")
+                    st.session_state["logged_in"] = True
+                    st.session_state["user"] = found_user
+                    st.session_state["role"] = found_user["role"]
+                    st.session_state["page"] = "home"
+
+
+                    time.sleep(2)
+                    st.rerun()
+
+
     # --- REGISTRATION ---
     st.subheader("New Admin Account")
     with st.container(border=True):
